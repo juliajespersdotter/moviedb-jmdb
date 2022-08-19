da478e3b17eb2f8a25936c9712bd61a94593cc9c
juliajespersdotter <79936455+juliajespersdotter@users.noreply.github.com>
2022-08-19 15:18:38 +0200

    Styled the movie cards with less borders, no rounded corners and more organised layout. Not finished.

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -6,15 +6,25 @@ const MovieCard = ({ movie }) => {
 	return (
 		<>
 			<Link to={`/movie/${movie.id}`}>
-				<Card>
-					<Card.Img
+				<Card border="light" text="dark" className="movieCard">
+					<img
 						variant="top"
+						className="mb-2"
 						src={`https://image.tmdb.org/t/p/w300/${posterPath}`}
 					/>
-					<Card.Body>
-						<Card.Text>{movie.title}</Card.Text>
-						<Card.Subtitle>{movie.release_date}</Card.Subtitle>
+					<Card.Body className="p-0">
+						<Card.Text className="mb-3 p-1">
+							{movie.title}
+						</Card.Text>
+						{/* <Card.Subtitle className="mb-2 text-muted">
+							{movie.release_date}
+						</Card.Subtitle> */}
 					</Card.Body>
+					<Card.Footer>
+						<small className="text-muted">
+							{movie.release_date}
+						</small>
+					</Card.Footer>
 				</Card>
 			</Link>
 		</>
