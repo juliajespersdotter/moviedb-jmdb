1fb25666483a2a39f004fd04657b11bbc4a896d8
juliajespersdotter <79936455+juliajespersdotter@users.noreply.github.com>
2022-08-26 16:54:17 +0200

    new moviecard file.

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,27 @@
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import useSlice from "../hooks/useSlice";
+
+const MovieCard = ({ movie }) => {
+	let posterPath = movie.poster_path;
+	const release_date = useSlice(movie.release_date);
+
+	return (
+		<>
+			<Link to={`/movie/${movie.id}`}>
+				<Card border="dark" className="bg-dark movie-card">
+					<img
+						className="mb-2 movie-img"
+						src={`https://image.tmdb.org/t/p/w300/${posterPath}`}
+					/>
+				</Card>
+				<p className="movie-text mt-2">{movie.title}</p>
+				<span className="subheading text-muted text-light">
+					{release_date}
+				</span>
+			</Link>
+		</>
+	);
+};
+
+export default MovieCard;
